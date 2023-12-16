19ef54008ab99fbac2de65956ec56b6bec11dec3
Nick <mrdoggydog37@gmail.com>
2023-12-16 08:49:15 +0100

    upd gitignore

--- Lab 3b/src/app.ts
+++ Lab 3b/src/app.ts
@@ -6,24 +6,24 @@ const firstJob = () => {
     });
 };
 
-firstJob()
-    .then(result => {
-        console.log(`1. ${result}`);
-    })
-    .catch(error => {
-        console.error(`1. Error: ${error}`);
-    });
+// firstJob()
+//     .then(result => {
+//         console.log(`1. ${result}`);
+//     })
+//     .catch(error => {
+//         console.error(`1. Error: ${error}`);
+//     });
 
-const handleFirstJob = async () => {
-    try {
-        const result = await firstJob();
-        console.log(`1. ${result}`);
-    } catch (error) {
-        console.error(`1. Error: ${error}`);
-    }
-};
+// // const handleFirstJob = async () => {
+// //     try {
+// //         const result = await firstJob();
+// //         console.log(`1. ${result}`);
+// //     } catch (error) {
+// //         console.error(`1. Error: ${error}`);
+// //     }
+// // };
 
-handleFirstJob();
+// // handleFirstJob();
 
 
 
@@ -35,24 +35,25 @@ const secondJob = () => {
     });
 };
 
-secondJob()
-    .then(result => {
-        console.log(`2. ${result}`);
-    })
-    .catch(error => {
-        console.error(`2. Error: ${error}`);
-    });
+// secondJob()
+//     .then(result => {
+//         console.log(`2. ${result}`);
+//     })
+//     .catch(error => {
+//         console.error(`2. Error: ${error}`);
+//     });
 
-const handleSecondJob = async () => {
-    try {
-        const result = await secondJob();
-        console.log(`2. ${result}`);
-    } catch (error) {
-        console.error(`2. Error: ${error}`);
-    }
-};
 
-handleSecondJob();
+// const handleSecondJob = async () => {
+//     try {
+//         const result = await secondJob();
+//         console.log(`2. ${result}`);
+//     } catch (error) {
+//         console.error(`2. Error: ${error}`);
+//     }
+// };
+
+// handleSecondJob();
 
 
 
@@ -72,24 +73,24 @@ const thirdJob = data => {
     });
 };
 
-thirdJob(3)
-    .then(result => {
-        console.log(`3. ${result}`);
-    })
-    .catch(error => {
-        console.error(`3. Error: ${error}`);
-    });
+// thirdJob(3)
+// .then(result => {
+//     console.log(`3. ${result}`);
+// })
+// .catch(error => {
+//     console.error(`3. Error: ${error}`);
+// });
 
-async function handleThirdJob() {
-    try {
-        const result = await thirdJob(4);
-        console.log(`3. ${result}`);
-    } catch (error) {
-        console.error(`3. Error: ${error}`);
-    }
-}
+// async function handleThirdJob() {
+//     try {
+//         const result = await thirdJob(4);
+//         console.log(`3. ${result}`);
+//     } catch (error) {
+//         console.error(`3. Error: ${error}`);
+//     }
+// }
 
-handleThirdJob();
+// handleThirdJob();
 
 
 
@@ -114,7 +115,6 @@ function createOrder(cardNumber) {
     });
 }
 
-// Функция для обработки платежа
 function proceedToPayment(orderNumber) {
     console.log(`Order number: ${orderNumber}`);
     return new Promise((resolve, reject) => {
@@ -128,32 +128,32 @@ function proceedToPayment(orderNumber) {
     });
 }
 
-createOrder("1234567890123456")
-    .then(orderNumber => proceedToPayment(orderNumber))
-    .then(result => console.log(result))
-    .catch(error => console.error(error));
+// createOrder("1234567890123456")
+// .then(orderNumber => proceedToPayment(orderNumber))
+// .then(result => console.log(result))
+// .catch(error => console.error(error));
 
-async function handleOrder() {
-    try {
-        const orderNumber = await createOrder("1234567890123456");
-        const result = await proceedToPayment(orderNumber);
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
-}
+// async function handleOrder() {
+//     try {
+//         const orderNumber = await createOrder("1234567890123456");
+//         const result = await proceedToPayment(orderNumber);
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
 
-handleOrder();
+// handleOrder();
 
 
 
 const squareNumber = number => {
     return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (typeof number !== "number") {
-				reject("Invalid input");
+        setTimeout(() => {
+            if (typeof number !== "number") {
+                reject("Invalid input");
 			} else {
-				resolve(Math.pow(number, 2));
+                resolve(Math.pow(number, 2));
 			}
 		}, 1000);
     });
@@ -161,11 +161,11 @@ const squareNumber = number => {
 
 const cubeNumber = number => {
     return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (typeof number !== "number") {
-				reject("Invalid input");
+        setTimeout(() => {
+            if (typeof number !== "number") {
+                reject("Invalid input");
 			} else {
-				resolve(Math.pow(number, 3));
+                resolve(Math.pow(number, 3));
 			}
 		}, 2000);
     });
@@ -173,31 +173,16 @@ const cubeNumber = number => {
 
 const fourthPowerNumber = number => {
     return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (typeof number !== "number") {
-				reject("Invalid input");
+        setTimeout(() => {
+            if (typeof number !== "number") {
+                reject("Invalid input");
 			} else {
-				resolve(Math.pow(number, 4));
+                resolve(Math.pow(number, 4));
 			}
 		}, 3000);
     });
 };
 
-Promise.all([squareNumber(2), cubeNumber(2), fourthPowerNumber(2)])
-    .then(results => console.log(`5. ${results}`))
-    .catch(error => console.error(`5. Error: ${error}`));
-
-Promise.all([squareNumber("two"), cubeNumber("two"), fourthPowerNumber("two")])
-    .then(results => console.log(`5. ${results}`))
-    .catch(error => console.error(`5. Error: ${error}`));
-
-Promise.race([squareNumber(2), cubeNumber(2), fourthPowerNumber(2)])
-    .then(results => console.log(`6. ${results}`))
-    .catch(error => console.error(`6. Error: ${error}`));
-
-(Promise as any).any([squareNumber(2), cubeNumber(2), fourthPowerNumber(2)])
-    .then(results => console.log(`6. ${results}`))
-    .catch(error => console.error(`6. Error: ${error}`));
 
 
 
@@ -214,18 +199,36 @@ function f3() {
 }
 
 function main() {
-    console.log(main);
-
+    console.log('main');
+    
     setTimeout(f1, 50);
     setTimeout(f3, 30);
-
+    
     new Promise((resolve, reject) => {
         resolve("Promise after f1 and f3");
     }).then(resolve => console.log(resolve));
-
+    
     new Promise((resolve, reject) => {
         resolve("Promise after promise");
     }).then(resolve => console.log(resolve));
+    
+    f2();
 }
+
+// Promise.all([squareNumber(2), cubeNumber(2), fourthPowerNumber(2)])
+// .then(results => console.log(`5. ${results}`))
+// .catch(error => console.error(`5. Error: ${error}`));
+
+// Promise.all([squareNumber("two"), cubeNumber("two"), fourthPowerNumber("two")])
+// .then(results => console.log(`5. ${results}`))
+// .catch(error => console.error(`5. Error: ${error}`));
+
+// Promise.race([squareNumber(2), cubeNumber(2), fourthPowerNumber(2)])
+// .then(results => console.log(`6. ${results}`))
+// .catch(error => console.error(`6. Error: ${error}`));
+
+// (Promise as any).any([squareNumber(2), cubeNumber(2), fourthPowerNumber(2)])
+// .then(results => console.log(`6. ${results}`))
+// .catch(error => console.error(`6. Error: ${error}`));
 
 main();
